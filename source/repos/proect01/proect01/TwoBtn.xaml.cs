4fc4a0b88f05a53300bb7d58cdb9716ce9e6fb35
Your Name <you@example.com>
2024-06-08 15:13:44 +0500

    New Pages

---
+++ source/repos/proect01/proect01/TwoBtn.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace proect01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TwoBtn : ContentPage
+    {
+        public TwoBtn()
+        {
+            InitializeComponent();
+        }
+        Button buttongame = new Button
+        {
+            Text = "Играть",
+            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+            BorderWidth = 1,
+            VerticalOptions = LayoutOptions.CenterAndExpand
+        };
+        Button buttonscore = new Button
+        {
+            Text = "Результаты",
+            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+            BorderWidth = 1,
+            VerticalOptions = LayoutOptions.CenterAndExpand
+        };
+    }
+}
