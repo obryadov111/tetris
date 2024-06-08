21782c545bf95e2ae4a6fe835d1e5fac6c3fcaa3
Your Name <you@example.com>
2024-06-08 14:33:21 +0500

    App.xaml.cs

--- source/repos/proect01/proect01/App.xaml.cs
+++ source/repos/proect01/proect01/App.xaml.cs
@@ -10,7 +10,7 @@ namespace proect01
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
